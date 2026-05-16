e479c7cb89cee8c9df59513bd12fb9c95beb4b51
Kaushal Shah <kaushal@enstead.com>
2026-05-17 01:01:40 +0530

    refactor(codegen): streamline GraphQL type generation and update scalar types
    - Removed commented-out plugin configurations for better clarity in codegen settings.
    - Updated scalar types for DateTime and JSON to use more appropriate definitions.
    - Enhanced organization of generated types for improved maintainability.

--- codegen.ts
+++ codegen.ts
@@ -8,78 +8,60 @@ const projectGenerates: Record<string, CodegenConfig['generates']> = {
   evora: {
     'src/plugins/common/booking-plugin/gql/generated.ts': {
       ...pluginConfig,
-      documents: ['src/plugins/common/booking-plugin/dashboard/**/*.graphql.ts'],
     },
-    'src/plugins/common/custom-customer-plugin/gql/generated.ts': {
-      ...pluginConfig,
-      documents: ['src/plugins/common/custom-customer-plugin/dashboard/**/*.graphql.ts'],
-    },
-    'src/plugins/common/custom-facet-plugin/gql/generated.ts': {
-      ...pluginConfig,
-      documents: ['src/plugins/common/custom-facet-plugin/dashboard/**/*.graphql.ts'],
-    },
-    'src/plugins/common/custom-product-plugin/gql/generated.ts': {
-      ...pluginConfig,
-      documents: ['src/plugins/common/custom-product-plugin/dashboard/**/*.graphql.ts'],
-    },
-    'src/plugins/common/custom-seller-plugin/gql/generated.ts': {
-      ...pluginConfig,
-      documents: ['src/plugins/common/custom-seller-plugin/dashboard/**/*.graphql.ts'],
-    },
-    'src/plugins/common/file-cache-plugin/gql/generated.ts': {
-      ...pluginConfig,
-      documents: ['src/plugins/common/file-cache-plugin/dashboard/**/*.graphql.ts'],
-    },
-    'src/plugins/common/geoanalytics-plugin/gql/generated.ts': {
-      ...pluginConfig,
-      documents: ['src/plugins/common/geoanalytics-plugin/dashboard/**/*.graphql.ts'],
-    },
-    'src/plugins/common/loyalty-points-plugin/gql/generated.ts': {
-      ...pluginConfig,
-      documents: ['src/plugins/common/loyalty-points-plugin/dashboard/**/*.graphql.ts'],
-    },
-    'src/plugins/common/marketplace-api-plugin/gql/generated.ts': {
-      ...pluginConfig,
-      documents: ['src/plugins/common/marketplace-api-plugin/dashboard/**/*.graphql.ts'],
-    },
-    'src/plugins/common/multivendor-plugin/gql/generated.ts': {
-      ...pluginConfig,
-      documents: ['src/plugins/common/multivendor-plugin/dashboard/**/*.graphql.ts'],
-    },
-    'src/plugins/common/pre-order-inquiry-plugin/gql/generated.ts': {
-      ...pluginConfig,
-      documents: ['src/plugins/common/pre-order-inquiry-plugin/dashboard/**/*.graphql.ts'],
-    },
-    'src/plugins/common/promotion-extension-plugin/gql/generated.ts': {
-      ...pluginConfig,
-      documents: ['src/plugins/common/promotion-extension-plugin/dashboard/**/*.graphql.ts'],
-    },
-    'src/plugins/common/qr-code/gql/generated.ts': {
-      ...pluginConfig,
-      documents: ['src/plugins/common/qr-code/dashboard/**/*.graphql.ts'],
-    },
-    'src/plugins/common/query-runner/gql/generated.ts': {
-      ...pluginConfig,
-      documents: ['src/plugins/common/query-runner/dashboard/**/*.graphql.ts'],
-    },
-    'src/plugins/common/review-plugin/gql/generated.ts': {
-      ...pluginConfig,
-      documents: ['src/plugins/common/review-plugin/dashboard/**/*.graphql.ts'],
-    },
-    'src/plugins/common/shared-plugin/gql/generated.ts': {
-      ...pluginConfig,
-      documents: ['src/plugins/common/shared-plugin/dashboard/**/*.graphql.ts'],
-    },
-    'src/plugins/common/wishlist-plugin/gql/generated.ts': {
-      ...pluginConfig,
-      documents: ['src/plugins/common/wishlist-plugin/dashboard/**/*.graphql.ts'],
-    },
+    // 'src/plugins/common/custom-customer-plugin/gql/generated.ts': {
+    //   ...pluginConfig,
+    // },
+    // 'src/plugins/common/custom-facet-plugin/gql/generated.ts': {
+    //   ...pluginConfig,
+    // },
+    // 'src/plugins/common/custom-product-plugin/gql/generated.ts': {
+    //   ...pluginConfig,
+    // },
+    // 'src/plugins/common/custom-seller-plugin/gql/generated.ts': {
+    //   ...pluginConfig,
+    // },
+    // 'src/plugins/common/file-cache-plugin/gql/generated.ts': {
+    //   ...pluginConfig,
+    // },
+    // 'src/plugins/common/geoanalytics-plugin/gql/generated.ts': {
+    //   ...pluginConfig,
+    // },
+    // 'src/plugins/common/loyalty-points-plugin/gql/generated.ts': {
+    //   ...pluginConfig,
+    // },
+    // 'src/plugins/common/marketplace-api-plugin/gql/generated.ts': {
+    //   ...pluginConfig,
+    // },
+    // 'src/plugins/common/multivendor-plugin/gql/generated.ts': {
+    //   ...pluginConfig,
+    // },
+    // 'src/plugins/common/pre-order-inquiry-plugin/gql/generated.ts': {
+    //   ...pluginConfig,
+    // },
+    // 'src/plugins/common/promotion-extension-plugin/gql/generated.ts': {
+    //   ...pluginConfig,
+    // },
+    // 'src/plugins/common/qr-code/gql/generated.ts': {
+    //   ...pluginConfig,
+    // },
+    // 'src/plugins/common/query-runner/gql/generated.ts': {
+    //   ...pluginConfig,
+    // },
+    // 'src/plugins/common/review-plugin/gql/generated.ts': {
+    //   ...pluginConfig,
+    // },
+    // 'src/plugins/common/shared-plugin/gql/generated.ts': {
+    //   ...pluginConfig,
+    // },
+    // 'src/plugins/common/wishlist-plugin/gql/generated.ts': {
+    //   ...pluginConfig,
+    // },
   },
   alcobaca: {},
   setubal: {
     'src/plugins/setubal/geoanalitycs-plugin/gql/generated.ts': {
       ...pluginConfig,
-      documents: ['src/plugins/setubal/geoanalitycs-plugin/dashboard/**/*.graphql.ts'],
     },
   },
   nobrinde: {
@@ -138,7 +120,7 @@ const config: CodegenConfig = {
   schema: 'http://localhost:3000/admin-api',
   config: {
     // This tells codegen that the `Money` scalar is a number
-    scalars: { Money: 'number' },
+    scalars: { Money: 'number', DateTime: Date },
     // This ensures generated enums do not conflict with the built-in types.
     namingConvention: { enumValues: 'keep' },
   },
